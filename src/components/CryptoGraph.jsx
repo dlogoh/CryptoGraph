221fa6ada8910ef8ac0439a8fe18d0db215ab7a4
dlogoh <davidlharris0@gmail.com>
2022-10-19 23:40:36 -0400

    Added date logic within CryptoGraph component

--- src/components/CryptoGraph.jsx
+++ src/components/CryptoGraph.jsx
@@ -43,23 +43,30 @@ function CryptoGraph({ coins }) {
       },
     },
   };
-  
-  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  
+
+  const thirtyDaysAgo = new Date(new Date().setDate(new Date().getDate() - 30));
+  const pastMonth = 
+    thirtyDaysAgo.getMonth() > 1 ? thirtyDaysAgo.getMonth() + 1 :
+    thirtyDaysAgo.getMonth() === 0 ? thirtyDaysAgo.getMonth() + 12 : thirtyDaysAgo.getMonth()
+  const pastDay = thirtyDaysAgo.getDate()
+  const pastYear = thirtyDaysAgo.getFullYear()
+
+  const currentDate = new Date()
+  const month = currentDate.getMonth()
+  const day = currentDate.getDate()
+  const year = currentDate.getFullYear()
+  const labels = []
+
+    console.log(pastMonth);
+
   const data = {
     labels,
     datasets: [
       {
-        label: 'Dataset 1',
+        label: 'Daily Price',
         data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
         borderColor: 'rgb(255, 99, 132)',
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      },
-      {
-        label: 'Dataset 2',
-        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
       },
     ],
   };
