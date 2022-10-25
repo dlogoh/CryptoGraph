bde5e8027e8b564cb47b16f6cb83a4b40eb105b1
gingydlh7477 <davidlharris0@gmail.com>
2022-10-24 23:18:37 -0400

    Added coingecko attribution

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import img from "../img/logo2.png";
+import gecko from "../img/coingecko.svg";
 
 function Navbar() {
   return (
     <nav className="navbar">
       <img src={img} alt="logo" className="nav-logo" />
+      <p className="powered-by">
+        Powered by:
+        <img src={gecko} alt="" />
+      </p>
     </nav>
   );
 }
